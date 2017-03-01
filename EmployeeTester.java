c0f5a68de54c5484b6464465d014e07aa2450697
steveorsino <steveorsino@yahoo.com>
2017-03-01 11:28:03 -0500

    updated driver class to test derived classes

--- EmployeeTester.java
+++ EmployeeTester.java
@@ -8,57 +8,128 @@ import java.util.Scanner;
 
 public class EmployeeTester {
 	public static void main (String args[]) {
-		//Create two instances of Employee with the two constructors
-		Employee myEmployee = new Employee();
-		Employee myEmployee2 = new Employee("Steve", "Orsino", "111-11-1111", "2152","IT", "IT156");
+		//Create two instances of PartTimeEmployee with the two constructors
+		PartTimeEmployee myPtEmployee = new PartTimeEmployee();
+		PartTimeEmployee myPtEmployee2 = new PartTimeEmployee("Steve", "Orsino", "111-11-1111", "2152","IT", "IT156", 35.0, 15.0);
+		
+		//Create two instances of FullTimeEmployee with the two constructors
+		FullTimeEmployee myFtEmployee = new FullTimeEmployee();
+		FullTimeEmployee myFtEmployee2 = new FullTimeEmployee("Kyle", "Hunter", "111-11-1111", "2152","IT", "IT156", 50000.0);
 		
 		//create scanner object
 		Scanner input = new Scanner(System.in);
 		//declare variables for attributes
 		String fName, lName, soc, sYear, dep, depCode;
+		double wkHrs = 0.0, hRate = 0.0, yrSal = 0.0;
 		
 		//test constructors and toString methods
 		System.out.println("\tDisplaying Employee default constructor:");
-		System.out.println(myEmployee.toString());
+		System.out.println(myPtEmployee);
 		System.out.println("\n\tDisplaying Employee parameter constructor: ");
-		System.out.println(myEmployee2.toString());
+		System.out.println(myPtEmployee2);
 		
-		//create another instance of Employee for accessors and mutators
-		Employee myEmployee3 = new Employee();
+		//test constructors and toString methods
+		System.out.println("\tDisplaying Employee default constructor:");
+		System.out.println(myFtEmployee);
+		System.out.println("\n\tDisplaying Employee parameter constructor: ");
+		System.out.println(myFtEmployee2);
+		
+		//create another instance of PartTimeEmployee for accessors and mutators
+		PartTimeEmployee myPtEmployee3 = new PartTimeEmployee();
 		System.out.println("\tDisplaying accessors and mutators: ");
-		System.out.print("Please Enter the first name of the new Employee: ");
+		System.out.print("Please Enter the first name of the new PartTimeEmployee: ");
 		fName = input.nextLine();
-		myEmployee3.setFName(fName);
-		System.out.println("You entered: " + myEmployee3.getFName());
+		myPtEmployee3.setFName(fName);
+		System.out.println("You entered: " + myPtEmployee3.getFName());
 		
-		System.out.print("Please Enter the last name of the new Employee: ");
+		System.out.print("Please Enter the last name of the new PartTimeEmployee: ");
 		lName = input.nextLine();
-		myEmployee3.setLName(lName);
-		System.out.println("You entered: " + myEmployee3.getLName());
+		myPtEmployee3.setLName(lName);
+		System.out.println("You entered: " + myPtEmployee3.getLName());
 		
-		System.out.print("Please Enter the SSN name of the new Employee: ");
+		System.out.print("Please Enter the SSN name of the new PartTimeEmployee: ");
 		soc = input.nextLine();
-		myEmployee3.setSocial(soc);
-		System.out.println("You entered: " + myEmployee3.getSocial());
+		myPtEmployee3.setSocial(soc);
+		System.out.println("You entered: " + myPtEmployee3.getSocial());
 		
-		System.out.print("Please Enter the start year of the new Employee: ");
+		System.out.print("Please Enter the start year of the new PartTimeEmployee: ");
 		sYear = input.nextLine();
-		myEmployee3.setSYear(sYear);
-		System.out.println("You entered: " + myEmployee3.getSYear());
+		myPtEmployee3.setSYear(sYear);
+		System.out.println("You entered: " + myPtEmployee3.getSYear());
 		
-		System.out.print("Please Enter the department of the new Employee: ");
+		System.out.print("Please Enter the department of the new PartTimeEmployee: ");
 		dep = input.nextLine();
-		myEmployee3.setDept(dep);
-		System.out.println("You entered: " + myEmployee3.getDept());
+		myPtEmployee3.setDept(dep);
+		System.out.println("You entered: " + myPtEmployee3.getDept());
 		
-		System.out.print("Please Enter the department code of the new Employee: ");
+		System.out.print("Please Enter the department code of the new PartTimeEmployee: ");
 		depCode = input.nextLine();
-		myEmployee3.setDeptCode(depCode);
-		System.out.println("You entered: " + myEmployee3.getDeptCode());
+		myPtEmployee3.setDeptCode(depCode);
+		System.out.println("You entered: " + myPtEmployee3.getDeptCode());
+		
+		System.out.print("Please Enter the weekly hours worked of the new PartTimeEmployee: ");
+		wkHrs = input.nextDouble();
+		input.nextLine();
+		myPtEmployee3.setWeeklyHoursWorked(wkHrs);
+		System.out.println("You entered: " + myPtEmployee3.getWeeklyHoursWorked());
+		
+		System.out.print("Please Enter the hourly pay rate of the new PartTimeEmployee: ");
+		hRate = input.nextDouble();
+		input.nextLine();
+		myPtEmployee3.setHourlyRate(hRate);
+		System.out.println("You entered: " + myPtEmployee3.getHourlyRate());
+		
+		System.out.println("Your PartTimeEmployee makes $" + myPtEmployee3.calculateWeeklyPay() + " Per Week");
+		
+		System.out.println("\nYour new PartTimeEmployee...");
+		System.out.println(myPtEmployee3);
 		
 		
-		System.out.println("\nYour new Employee...");
-		System.out.println(myEmployee3.toString());
+		//create another instance of PartTimeEmployee for accessors and mutators
+
+		FullTimeEmployee myFtEmployee3 = new FullTimeEmployee();
+		System.out.println("\tDisplaying accessors and mutators: ");
+		System.out.print("Please Enter the first name of the new FullTimeEmployee: ");
+		fName = input.nextLine();
+		myFtEmployee3.setFName(fName);
+		System.out.println("You entered: " + myFtEmployee3.getFName());
+		
+		System.out.print("Please Enter the last name of the new FullTimeEmployee: ");
+		lName = input.nextLine();
+		myFtEmployee3.setLName(lName);
+		System.out.println("You entered: " + myFtEmployee3.getLName());
+		
+		System.out.print("Please Enter the SSN name of the new FullTimeEmployee: ");
+		soc = input.nextLine();
+		myFtEmployee3.setSocial(soc);
+		System.out.println("You entered: " + myFtEmployee3.getSocial());
+		
+		System.out.print("Please Enter the start year of the new FullTimeEmployee: ");
+		sYear = input.nextLine();
+		myFtEmployee3.setSYear(sYear);
+		System.out.println("You entered: " + myFtEmployee3.getSYear());
+		
+		System.out.print("Please Enter the department of the new FullTimeEmployee: ");
+		dep = input.nextLine();
+		myFtEmployee3.setDept(dep);
+		System.out.println("You entered: " + myFtEmployee3.getDept());
+		
+		System.out.print("Please Enter the department code of the new FullTimeEmployee: ");
+		depCode = input.nextLine();
+		myFtEmployee3.setDeptCode(depCode);
+		System.out.println("You entered: " + myFtEmployee3.getDeptCode());
+		
+		System.out.print("Please Enter the annual salary of the new FullTimeEmployee: ");
+		yrSal = input.nextDouble();
+		myFtEmployee3.setAnnualSalary(yrSal);
+		System.out.println("You entered: " + myFtEmployee3.getAnnualSalary());
+		
+		System.out.println("Your FullTimeEmployee makes $" + myFtEmployee3.calculateWeeklyPay() + " Per Week");
+		
+		System.out.println("\nYour new FullTimeEmployee...");
+		System.out.println(myFtEmployee3);
+		
+		
 		input.close();
 	}
 }
